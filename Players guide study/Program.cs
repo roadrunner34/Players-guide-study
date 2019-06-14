1d4ab6e5b815320f4d7ae51ffc9753ba6f57f9e6
David Romero <DRomero@dealersocket.com>
2019-06-14 08:12:11 -0600

    Update Program.cs
    added comments about chapters, started work on type chapter 6.

--- Players guide study/Program.cs
+++ Players guide study/Program.cs
@@ -11,6 +11,8 @@ namespace Players_guide_study
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            //Code below is from chapter 5 about variables
             int score;
             score = 0;
             int meaningoflife = 42;
@@ -28,6 +30,19 @@ namespace Players_guide_study
 
             Console.WriteLine("B value = " +b);
             Console.WriteLine("A Value = " +a);
+
+            //code below is from chapter 6 about types
+            byte asinglebyte = 34;
+            short asingleshort = 12345;
+            long averybignumber = 801706373123861;
+
+            Console.WriteLine("byte: " +asinglebyte +", short: "+asingleshort+", long: "+averybignumber);
+            asinglebyte = 244;
+            asingleshort = -23861;
+            averybignumber = -1234568485455;
+            Console.WriteLine("Behind the scenes stuff occurs");
+            Console.WriteLine("byte: " + asinglebyte + ", short: " + asingleshort + ", long: " + averybignumber);
+
 
 
         }
